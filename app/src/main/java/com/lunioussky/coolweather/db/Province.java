3bceafc27f1dba4163d78a076e1ad866825ce980
lunious <1164503275@qq.com>
2017-05-23 10:00:24 +0800

    创建数据库和表，以及在项目中的各项配置。

---
+++ app/src/main/java/com/lunioussky/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.lunioussky.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lunioussky on 2017/5/23.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
